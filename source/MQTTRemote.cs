65ec88f98eadb3c5d78aeae6f1448f9b15f1ee91
Steve Upton <steve@steveupton.net>
2013-07-31 22:17:52 +0100

    Initial commit

---
+++ source/MQTTRemote.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MqttLib;
+
+public class MQTTRemote : Part {
+
+     //static IMqtt _client;
+
+    protected override void onFlightStart()  //Called when vessel is placed on the launchpad
+    {
+        
+        //base.onFlightStart();
+        print("on flight start");
+
+        IMqtt _client;
+        _client = MqttClientFactory.CreateClient("tcp://m2m.eclipse.org:1883", "ksptest1");
+        _client.PublishArrived += new PublishArrivedDelegate(client_PublishArrived);
+        print("MQTT Client created");
+        _client.Connect(true);
+        print("MQTT Client connected");
+        _client.Publish("ksp/test1/telemetry/blah", "Connected!", QoS.BestEfforts, false);
+        print("MQTT Message published");
+
+        _client.Subscribe("ksp/test1/commands/#", QoS.BestEfforts);   // Subscribe to command topic
+        print("MQTT Client subscribed to command topic");
+
+        // Create the client
+        /*_client = MqttClientFactory.CreateClient("m2m.eclipse.org:1883", "ksptest1");
+
+        // Setup callbacks
+        _client.Connected += new ConnectionDelegate(client_Connected);
+        _client.PublishArrived += new PublishArrivedDelegate(client_PublishArrived);
+        _client.ConnectionLost += new ConnectionDelegate(_client_ConnectionLost);
+
+        _client.Connect(true);  // Connect
+        _client.Subscribe("ksp/test1/commands/#", QoS.BestEfforts);   // Subscribe to command topic*/
+
+        //ksp/test1/telemetry/#
+        //at the beginning of the flight, register your fly-by-wire control function that will be called repeatedly during flight:
+        //FlightInputHandler.OnFlyByWire += new FlightInputHandler.FlightInputCallback(fly);
+    }
+
+    bool client_PublishArrived(object sender, PublishArrivedArgs e)
+    {
+        print("Received MQTT Message");
+        print("Topic: " + e.Topic);
+        print("Payload: " + e.Payload);
+
+        Staging.ActivateNextStage();
+
+        return true;
+    }
+
+    /*void _client_ConnectionLost(object sender, EventArgs e)
+    {
+        print("MQTT connection lost");
+    }*/
+
+}
+
