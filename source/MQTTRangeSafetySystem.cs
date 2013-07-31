95ac25ea9df8366fff386d35029596d836ce992a
Steve Upton <steve@steveupton.net>
2013-08-01 00:04:03 +0100

    Adding basic Range Safety System

--- source/MQTTRangeSafetySystem.cs
+++ source/MQTTRangeSafetySystem.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEngine;
 using MqttLib;
 
 namespace MQTTKSP {
 
-    public class MQTTRemote : Part
+    class MQTTRangeSafetySystem : Part
     {
 
         protected override void onFlightStart()  //Called when vessel is placed on the launchpad
@@ -25,25 +24,9 @@ namespace MQTTKSP {
             print("MQTT Client connected");
             _client.Publish("ksp/test1/telemetry/blah", "Connected!", QoS.BestEfforts, false);
             print("MQTT Message published");
-            _client.Subscribe("ksp/test1/commands/#", QoS.BestEfforts);   // Subscribe to command topic
+            _client.Subscribe("ksp/test1/cmd/rss", QoS.BestEfforts);   // Subscribe to rss command topic
             print("MQTT Client subscribed to command topic");
 
-
-
-            // Create the client
-            /*_client = MqttClientFactory.CreateClient("m2m.eclipse.org:1883", "ksptest1");
-
-            // Setup callbacks
-            _client.Connected += new ConnectionDelegate(client_Connected);
-            _client.PublishArrived += new PublishArrivedDelegate(client_PublishArrived);
-            _client.ConnectionLost += new ConnectionDelegate(_client_ConnectionLost);
-
-            _client.Connect(true);  // Connect
-            _client.Subscribe("ksp/test1/commands/#", QoS.BestEfforts);   // Subscribe to command topic*/
-
-            //ksp/test1/telemetry/#
-            //at the beginning of the flight, register your fly-by-wire control function that will be called repeatedly during flight:
-            //FlightInputHandler.OnFlyByWire += new FlightInputHandler.FlightInputCallback(fly);
         }
 
         bool client_PublishArrived(object sender, PublishArrivedArgs e)
@@ -52,15 +35,11 @@ namespace MQTTKSP {
             print("Topic: " + e.Topic);
             print("Payload: " + e.Payload);
 
-            Staging.ActivateNextStage();
+            parent.explode();
+            this.explode();
 
             return true;
         }
-
-        /*void _client_ConnectionLost(object sender, EventArgs e)
-        {
-            print("MQTT connection lost");
-        }*/
 
     }
 
