edd0a4d54e06bc43a9d497e41e807479b5eca379
Steve Upton <steve@steveupton.net>
2013-08-01 23:30:51 +0100

    Added new MQTTPartModule superclass and MQTTTelemetryPublisher part

---
+++ source/MQTTPartModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MqttLib;
+
+namespace MQTTKSP
+{
+    class MQTTPartModule : PartModule {
+
+        public IMqtt client;
+
+        public override void OnStart(PartModule.StartState state)
+        {
+            System.Random rand = new System.Random();
+            String clientID = "ksp" + rand.Next(100000);
+
+            client = MqttClientFactory.CreateClient("tcp://m2m.eclipse.org:1883", clientID);
+            client.Connect(true);
+            print("MQTT Client connected");
+            client.Publish("ksp/test1/tlm/alerts", "Connected!", QoS.BestEfforts, false);
+        }
+
+    }
+}
---
+++ source/MQTTTelemetryPublisher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MqttLib;
+
+namespace MQTTKSP
+{
+    class MQTTTelemetryPublisher : MQTTPartModule
+    {
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            client.Publish("ksp/test1/tlm/acceleration", part.vessel.acceleration.magnitude.ToString(), QoS.BestEfforts, false);
+            client.Publish("ksp/test1/tlm/g", part.vessel.geeForce.ToString(), QoS.BestEfforts, false);
+            client.Publish("ksp/test1/tlm/altitude", part.vessel.altitude.ToString(), QoS.BestEfforts, false);
+        }
+
+    }
+}
