7d2a17ea703de9ff7e9ec2dbb8112e7ad8dee17b
Steve Upton <steve@steveupton.net>
2013-08-03 19:06:00 +0100

    Switching Remote Control and Range Safety to use MQTTPartModule. Renaming Remote Control class.

--- source/MQTTRemoteControl.cs
+++ source/MQTTRemoteControl.cs
@@ -8,21 +8,20 @@ using MqttLib;
 namespace MQTTKSP
 {
 
-    class MQTTRangeSafetySystem : MQTTPartModule
+    class MQTTRemoteControl : MQTTPartModule
     {
 
         public override void OnStart(PartModule.StartState state)
         {
             base.OnStart(state);
             client.PublishArrived += new PublishArrivedDelegate(client_PublishArrived);
-            client.Subscribe("ksp/test1/cmd/rss", QoS.BestEfforts);   // Subscribe to rss command topic
-            print("Range Safety System subscribed to command topic");
+            client.Subscribe("ksp/test1/cmd/rc", QoS.BestEfforts);   // Subscribe to rss command topic
+            print("Remote Control System subscribed to command topic");
         }
 
         bool client_PublishArrived(object sender, PublishArrivedArgs e)
         {
-            part.parent.explode();
-            part.explode();
+            Staging.ActivateNextStage();
             return true;
         }
 
